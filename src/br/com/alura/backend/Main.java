e10141b6be23aa2ab5771bd237edb38f0f92a995
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-03-24 17:13:55 -0300

    dividindo o json por title e urlImage da lista de filmes

--- src/br/com/alura/backend/Main.java
+++ src/br/com/alura/backend/Main.java
@@ -1,16 +1,53 @@
 package br.com.alura.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 import br.com.alura.backend.movies.SearchMovies;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		SearchMovies listMovies = new SearchMovies(/*Link da Api da IMDB*/);
+		SearchMovies listMovies = new SearchMovies("https://imdb-api.com/en/API/Top250Movies/k_mjiovcac");
 		String movies = listMovies.search();
 		
+		//titles
+		String[] titlessplited = movies.split("title\":\"");
 		
-		System.out.println(movies);
+		List<String> titles = new ArrayList<String>();
+		
+		for (String title : titlessplited) {
+			String [] titlesplit = title.split("\",\"full");
+			titles.add(titlesplit[0]);
+		}
+		
+		titles.remove(0);
+		
+		titles.forEach(new Consumer<String>() {
+			public void accept(String t) {
+				System.out.println(t);
+			}
+		});
+		
+		//images
+		String[] imagessplited = movies.split("image\":\"");
+		
+		List<String> images = new ArrayList<String>();
+		
+		for (String image : imagessplited) {
+			String [] imagesplit = image.split("\",\"crew");
+			images.add(imagesplit[0]);
+		}
+		
+		images.remove(0);
+		
+		images.forEach(new Consumer<String>() {
+			public void accept(String i) {
+				System.out.println(i);
+			}
+		});
 
 	}
 
